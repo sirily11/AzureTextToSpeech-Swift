17220472db8e595726449f8f104d71b245c57b7e
sirily11 <sirily1997@gmail.com>
2023-02-06 11:24:26 +0800

    fix: update init

--- Sources/AzureTextToSpeech/Types/AzureVoice.swift
+++ Sources/AzureTextToSpeech/Types/AzureVoice.swift
@@ -17,7 +17,7 @@ public struct AzureVoice: Codable, Hashable {
     public let status: String?
     public let extendedPropertyMap: [String: String]?
     public let wordsPerMinute: String?
-
+    
     enum CodingKeys: String, CodingKey {
         case name = "Name"
         case displayName = "DisplayName"
@@ -36,4 +36,39 @@ public struct AzureVoice: Codable, Hashable {
     }
     
     public static var defaultValue: AzureVoice = AzureVoice(name: "Loading...", displayName: "Loading...", localName: "Loading...", shortName: "Loading...", gender: "Loading...", locale: "Loading...", localeName: "Loading...", styleList: nil, rolePlayList: nil, sampleRateHertz: nil, voiceType: "Loading...", status: nil, extendedPropertyMap: nil, wordsPerMinute: nil)
+    
+    
+    public init(name: String, displayName: String, localName: String, shortName: String, gender: String, locale: String, localeName: String, styleList: [String]?, rolePlayList: [String]?, sampleRateHertz: String?, voiceType: String, status: String?, extendedPropertyMap: [String: String]?, wordsPerMinute: String?) {
+        self.name = name
+        self.displayName = displayName
+        self.localName = localName
+        self.shortName = shortName
+        self.gender = gender
+        self.locale = locale
+        self.localeName = localeName
+        self.styleList = styleList
+        self.rolePlayList = rolePlayList
+        self.sampleRateHertz = sampleRateHertz
+        self.voiceType = voiceType
+        self.status = status
+        self.extendedPropertyMap = extendedPropertyMap
+        self.wordsPerMinute = wordsPerMinute
+    }
+    
+    public init(shortName: String, locale: String, gender: String) {
+        self.shortName = shortName
+        self.name = ""
+        self.locale = locale
+        self.displayName = shortName
+        self.gender = gender
+        self.localName = ""
+        self.voiceType = ""
+        self.extendedPropertyMap = nil
+        self.rolePlayList = nil
+        self.status = nil
+        self.wordsPerMinute = nil
+        self.localeName = ""
+        self.styleList = nil
+        self.sampleRateHertz = nil
+    }
 }
